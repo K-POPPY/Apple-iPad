9ec19bfcf5e6f6fdffb2c87751d63c82c51dfb76
POPPY <yeareung.poppy@gmail.com>
2023-04-10 19:33:59 +0900

    반응형 레이아웃 작업

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
-[![vercel배포](https://img.shields.io/badge/Vercel-000000?style=for-the-badge&logo=Vercel&logoColor=white)](https://vercel.com/k-poppy/apple-ipad)
+[![vercel배포](https://img.shields.io/badge/Vercel-000000?style=plastic&logo=Vercel&logoColor=white)](https://vercel.com/k-poppy/apple-ipad)
 # Apple iPad 10.2 (KR) _ Clone Coding
 
-[![AppleLogo](https://apple-ipad-azure.vercel.app/images/environment_icon.png)](https://apple-ipad-azure.vercel.app/) - iPad
+[![iPadLogo](https://apple-ipad-azure.vercel.app/images/hero_ipad_text.png)](https://apple-ipad-azure.vercel.app/)
+[![AppleLogo](https://apple-ipad-azure.vercel.app/images/environment_icon.png)](https://apple-ipad-azure.vercel.app/) 
 
 <a href="https://apple-ipad-azure.vercel.app/">Main Page</a> 
 
+- PC, 태블릿, 모바일 반응형
 - 웹표준과 웹접근성을 준수 / 시멘틱마크업 & BEM 네이밍규칙 사용
 - 검색아이콘 클릭시 검색바 노출과 헤더 메뉴 사라지는 애니매이션 구현, 검색바 빠져나가면 검색바에 타이핑 했던 텍스트 초기화
 - 장바구니 아이콘 토글, `event.stopPropagation()`을 사용해 클릭하면 닫히는 이벤트버블링을 장바구니 아이콘과 장바구니메뉴리스트 영역에서 멈춰 장바구니메뉴리스트를 제외하고 뷰포트 모든 영역 클릭시 장바구니메뉴리스트 닫힐수있게 구현
@@ -14,5 +16,5 @@
 - `new IntersectionObserver()`를 사용하여 각 섹션의 `info`영역이 화면에 들어오면 나타나는 효과 구현 
 - '촬영에서 편집 공유까지 한 기기에서.' 영역의 `video`를 CSS `mask-image`로 마스크및 html 속성으로 제어, 스크립트로 재생/일시정지 버튼 제어
 - '당신에게 맞는 iPad는?' 영역 상품이미지, 컬러, 정보, 가격등을 `ipads.js`의 데이터를 기반으로 `javascript`로 화면 노출
-- 주의사항 각주 삽입
+- 각주 삽입해 클릭시 footer의 주의사항으로 이동
 - footer navigation 영역 텍스트와 url을 `navigations.js`의 데이터를 기반으로 `javascript`로 화면 노출
--- js/main.js
+++ js/main.js
@@ -38,12 +38,15 @@ const searchInputEl = searchWrapEl.querySelector('input');
 const searchDelayEls = [...searchWrapEl.querySelectorAll('li')];
 
 searchStarterEl.addEventListener('click', showSearch);
-searchCloserEl.addEventListener('click', hideSearch);
+searchCloserEl.addEventListener('click', function (event) {
+  event.stopPropagation();
+  hideSearch();
+});
 searchShadowEl.addEventListener('click', hideSearch);
 
 function showSearch() {
   headerEl.classList.add('searching');
-  document.documentElement.classList.add('fixed');
+  stopScroll();
   headerMenuEls.reverse().forEach(function (el, index) {
     el.style.transitionDelay = `${(index * 0.4) / headerMenuEls.length}s`;
   });
@@ -56,7 +59,7 @@ function showSearch() {
 }
 function hideSearch() {
   headerEl.classList.remove('searching');
-  document.documentElement.classList.remove('fixed');
+  playScroll();
   headerMenuEls.reverse().forEach(function (el, index) {
     el.style.transitionDelay = `${(index * 0.4) / headerMenuEls.length}s`;
   });
@@ -66,6 +69,71 @@ function hideSearch() {
   searchDelayEls.reverse();
   searchInputEl.value = '';
 }
+function playScroll() {
+  document.documentElement.classList.remove('fixed');
+}
+function stopScroll() {
+  document.documentElement.classList.add('fixed');
+}
+
+// 헤더 메뉴 토글 [모바일]
+const menuStarterEl = headerEl.querySelector('.menu-starter');
+
+menuStarterEl.addEventListener('click', function () {
+  if (headerEl.classList.contains('menuing')) {
+    headerEl.classList.remove('menuing');
+    searchInputEl.value = '';
+    playScroll();
+  } else {
+    headerEl.classList.add('menuing');
+    stopScroll();
+  }
+});
+
+// 헤더 검색 [모바일]
+const searchTextFieldEl = headerEl.querySelector('.textfield');
+const searchCancelerEl = headerEl.querySelector('.search-canceler');
+
+searchTextFieldEl.addEventListener('click', function () {
+  headerEl.classList.add('searching--mobile');
+  searchInputEl.focus();
+});
+searchCancelerEl.addEventListener('click', function () {
+  headerEl.classList.remove('searching--mobile');
+});
+
+// 화면크기가 달라졌을 때 검색 모드 종료
+window.addEventListener('resize', function () {
+  if (window.innerWidth <= 740) {
+    headerEl.classList.remove('searching');
+  } else {
+    headerEl.classList.remove('searching--mobile');
+  }
+});
+
+// 네비게이션 메뉴 토글 [모바일]
+const navEl = document.querySelector('nav');
+const navMenuToggleEl = navEl.querySelector('.menu-toggler');
+const navMenuShadowEl = navEl.querySelector('.shadow');
+
+navMenuToggleEl.addEventListener('click', function () {
+  if (navEl.classList.contains('menuing')) {
+    hideNavMenu();
+  } else {
+    showNavMenu();
+  }
+});
+navEl.addEventListener ('click', function (event) {
+  event.stopPropagation();
+});
+navMenuShadowEl.addEventListener('click', hideNavMenu);
+window.addEventListener('click', hideNavMenu);
+function showNavMenu () {
+  navEl.classList.add('menuing');
+};
+function hideNavMenu () {
+  navEl.classList.remove('menuing');
+};
 
 // 요소의 가시성 관찰
 const io = new IntersectionObserver(function (entries) {
@@ -100,7 +168,7 @@ pauseBtn.addEventListener('click', function () {
   pauseBtn.classList.add('hide');
 });
 
-// '당신에게 맞는 iPad는?' 랜더링
+// '당신에게 맞는 iPad는?' 제품정보 랜더링
 const itemsEl = document.querySelector('section.compare .items');
 
 ipads.forEach(function (ipad) {
@@ -109,7 +177,7 @@ ipads.forEach(function (ipad) {
 
   let colorList = '';
   ipad.colors.forEach(function (color) {
-    colorList += `<li style="background-color: ${color};"></li>`
+    colorList += `<li style="background-color: ${color};"></li>`;
   });
 
   itmeEl.innerHTML = /* HTML */ `
@@ -129,28 +197,29 @@ ipads.forEach(function (ipad) {
   itemsEl.append(itmeEl);
 });
 
-// Footer navigations
+// Footer navigations 렌더링
 const navigationsEl = document.querySelector('footer .navigations');
 
-navigations.forEach(function(nav){
+navigations.forEach(function (nav) {
   const mapEl = document.createElement('div');
   mapEl.classList.add('map');
 
   let mapList = '';
-  nav.maps.forEach(function(map){
+  nav.maps.forEach(function (map) {
     mapList += /* html */ `<li>
       <a href="${map.url}">${map.name}</a>
-    </li>`
+    </li>`;
   });
 
   mapEl.innerHTML = /* html */ `
     <h3>
       <span class="text">${nav.title}</span>
+      <span class="icon">+</span>
     </h3>
     <ul>
       ${mapList}
     </ul>
-  `
+  `;
 
   navigationsEl.append(mapEl);
 });
@@ -158,3 +227,13 @@ navigations.forEach(function(nav){
 // copyright 현재 년도
 const thisYearEl = document.querySelector('span.this-year');
 thisYearEl.textContent = new Date().getFullYear();
+
+// map 아코디언 메뉴 토글 [모바일]
+const mapEls = navigationsEl.querySelectorAll('.map');
+
+mapEls.forEach(function (el) {
+  const h3El = el.querySelector('h3');
+  h3El.addEventListener('click', function () {
+    el.classList.toggle('active');
+  });
+});
